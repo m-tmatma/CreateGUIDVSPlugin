28ad1bb0e02944efc688805e2cbec571dd687573
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-03-31 19:18:22 +0900

    remove unnecessary 'using'.

--- CreateGUIDVSPlugin/RenewGuidCommand.cs
+++ CreateGUIDVSPlugin/RenewGuidCommand.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.ComponentModel.Design;
-using System.Globalization;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace CreateGUIDVSPlugin
 {
