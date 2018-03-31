71c6ca59f7ce63a3f2374ebc49196c52b329543f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-03-31 19:50:52 +0900

    fix SA1200 warning. CSharp.Ordering : All using directives must be placed inside of the namespace.

--- CreateGUIDVSPlugin/ReplaceWithNewGuid.cs
+++ CreateGUIDVSPlugin/ReplaceWithNewGuid.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-
-namespace CreateGUIDVSPlugin
+﻿namespace CreateGUIDVSPlugin
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
     public class ReplaceWithNewGuid
     {
         /// <summary>
