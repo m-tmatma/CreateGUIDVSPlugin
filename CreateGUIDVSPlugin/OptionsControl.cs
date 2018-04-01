7c4744feefc0ad9d811a99c58276db8de311a16f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-01 09:25:54 +0900

    move using to inside of namespace.

--- CreateGUIDVSPlugin/OptionsControl.cs
+++ CreateGUIDVSPlugin/OptionsControl.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CreateGUIDVSPlugin
+﻿namespace CreateGUIDVSPlugin
 {
+    using System;
+    using System.Windows.Forms;
+
     public partial class OptionsControl : UserControl
     {
         private OptionsPageSetting settingOptionsPage;
