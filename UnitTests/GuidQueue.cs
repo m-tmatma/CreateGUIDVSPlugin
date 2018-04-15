dde1f91f1c1bd10f2f26c6f61c0d89bef9b3832e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-04-15 18:29:38 +0900

    #24: add GuidQueue and its test class.

---
+++ UnitTests/GuidQueue.cs
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GuidQueue.cs" company="Masaru Tsuchiyama">
+//     Copyright (c) Masaru Tsuchiyama. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Tester Class for providing custom GUID generator 
+    /// </summary>
+    internal class GuidQueue
+    {
+        /// <summary>
+        /// delegate for creating new GUID
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        internal delegate Guid NewGuid();
+
+        /// <summary>
+        /// seed to create a new GUID.
+        /// </summary>
+        private Queue<Guid> fifo;
+
+        /// <summary>
+        /// delegate for creating brand-new GUID
+        /// </summary>
+        private NewGuid newGuid;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GuidQueue" /> class.
+        /// </summary>
+        public GuidQueue(NewGuid newGuid = null)
+        {
+            this.fifo = new Queue<Guid>();
+            if (newGuid == null)
+            {
+                this.newGuid = delegate () { return Guid.NewGuid(); };
+            }
+            else
+            {
+                this.newGuid = newGuid;
+            }
+        }
+
+        /// <summary>
+        /// create brand-new GUID
+        /// </summary>
+        /// <returns>new GUID</returns>
+        public Guid BrandNewGuid()
+        {
+            var guid = this.newGuid();
+            this.fifo.Enqueue(guid);
+            return guid;
+        }
+
+        /// <summary>
+        /// create new GUID from cache.
+        /// </summary>
+        /// <returns>new GUID</returns>
+        public Guid NewGuidFromCache()
+        {
+            var guid = this.fifo.Dequeue();
+            return guid;
+        }
+    }
+}
---
+++ UnitTests/TestGuidQueue.cs
@@ -0,0 +1,83 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestGuidQueue.cs" company="Masaru Tsuchiyama">
+//     Copyright (c) Masaru Tsuchiyama. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace UnitTests
+{
+    using System;
+    using System.Text;
+    using Microsoft.Win32;
+    using NUnit.Framework;
+    using CreateGUIDVSPlugin.Utility;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// unit test for GuidQueue
+    /// </summary>
+    [TestFixture]
+    public class TestGuidQueue
+    {
+        /// <summary>
+        /// GUID queue
+        /// </summary>
+        private GuidQueue guidQueue;
+
+        /// <summary>
+        /// setup method to be called to initialize unit tests
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            this.guidQueue = new GuidQueue();
+        }
+
+        /// <summary>
+        /// cleanup method to be called to cleanup unit tests
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            this.guidQueue = null;
+        }
+
+        /// <summary>
+        /// Test for repeating GUID generation One by One
+        /// </summary>
+        /// <param name="count">loop count</param>
+        [TestCase(3)]
+        [TestCase(10)]
+        public void TestRepeatOneByOne(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var guid1 = this.guidQueue.BrandNewGuid();
+                var guid2 = this.guidQueue.NewGuidFromCache();
+                Assert.That(guid1, Is.EqualTo(guid2));
+            }
+            Assert.That(() => { this.guidQueue.NewGuidFromCache(); }, Throws.TypeOf<InvalidOperationException>());
+        }
+
+
+        /// <summary>
+        /// Test for repeating GUID generation all together
+        /// </summary>
+        /// <param name="count">loop count</param>
+        [TestCase(3)]
+        [TestCase(10)]
+        public void TestRepeatAllTogether(int count)
+        {
+            var guids = new Guid[count];
+            for (int i = 0; i < count; i++)
+            {
+                guids[i] = this.guidQueue.BrandNewGuid();
+            }
+            for (int i = 0; i < count; i++)
+            {
+                var guid2 = this.guidQueue.NewGuidFromCache();
+                Assert.That(guids[i], Is.EqualTo(guid2));
+            }
+            Assert.That(() => { this.guidQueue.NewGuidFromCache(); }, Throws.TypeOf<InvalidOperationException>());
+        }
+    }
+}
